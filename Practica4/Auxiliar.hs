7ef62972117cfab9c0354bfc687aa4e56af514d3
maestramarbarrios-tech <maestra.mar.barrios@gmail.com>
2026-03-26 19:03:37 -0600

    Create Auxiliar.hs as a module for auxiliary helper functions

---
+++ Practica4/Auxiliar.hs
@@ -0,0 +1,8 @@
+module Auxiliar where
+
+-- Example auxiliary function
+-- This function adds two numbers
+addNumbers :: Int -> Int -> Int
+addNumbers x y = x + y
+
+-- You can add more auxiliary helper functions here
