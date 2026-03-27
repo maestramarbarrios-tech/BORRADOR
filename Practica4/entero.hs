1675413c72d1ad2f419bc638f8bb75cf1f3def7b
maestramarbarrios-tech <maestra.mar.barrios@gmail.com>
2026-03-26 19:03:19 -0600

    Create entero.hs with multiEnt function implementation

---
+++ Practica4/entero.hs
@@ -0,0 +1,10 @@
+-- Implementation of multiEnt function
+-- This function calculates the multi-connective equivalence for given integer inputs.
+-- It takes in two integers and returns their product as the result.
+
+-- Examples:
+-- multiEnt 3 5 produces 15
+-- multiEnt (-4) 6 produces -24
+
+multiEnt :: Int -> Int -> Int
+multiEnt a b = a * b
